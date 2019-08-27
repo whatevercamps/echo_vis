fcdbe7c6eabd2593616f985f1926bb3ca4da2d50
JUAN SEBASTIAN PRIETO BUSTAMANTE <js.prieto10@isc334.uniandes.edu.co>
2019-08-27 14:08:33 -0500

    scroll y filtros

--- filtros.js
+++ filtros.js
@@ -6,31 +6,56 @@ var one = true;
 
 $("#male").click(d => {
     male = !male;
-    console.log(male);
+    if (male) $("#male").addClass('btn-hot')
+    else $("#male").removeClass('btn-hot')
 })
 
 $("#female").click(d => {
     female = !female;
-    console.log(female);
+    if (female) $("#female").addClass('btn-hot')
+    else $("#female").removeClass('btn-hot')
 });
+$("#edge").click(d => {
+    edge = !edge;
+    if (edge) $("#edge").addClass('btn-hot')
+    else $("#edge").removeClass('btn-hot')
+})
+
+$("#vibe").click(d => {
+    vibe = !vibe;
+    if (vibe) $("#vibe").addClass('btn-hot')
+    else $("#vibe").removeClass('btn-hot')
+});
+$("#one").click(d => {
+    one = !one;
+    if (one) $("#one").addClass('btn-hot')
+    else $("#one").removeClass('btn-hot')
+})
 
 
 $("#apply").click(d => {
+    console.log([male, female, edge, vibe, one])
     sexos = [];
     male ? sexos.push("Hombre") : "";
     female ? sexos.push("Mujer") : "";
-    edades = ["60 a 64"];
-    respuesta = [0, 1, 2];
+
+    edades = [];
+
+    edge ? edades.push('jovenes') : ''
+    vibe ? edades.push('adultos') : ''
+    one ? edades.push('mayores') : ''
+
+    respuesta = [0];
     numero = 40;
-    corregimientos = ["O5) SANTA ELENA"];
+    corregimientos = ["Corregimientos", "4) Aranjuez", "16) Belén", "15) Guayabal", "NaN", "11) Laureles-Estadio", "12) La América", "5) Castilla", "7) Robledo", "6) Doce de Octubre", "13) San Javier", "1) Popular", "9) Buenos Aires", "3) Manrique", "2) Santa Cruz", "8) Villa Hermosa", "10) La Candelaria", "14) El Poblado", "otros"];
     req = { sexos: sexos, edades: edades, corregimientos: corregimientos, respuesta: respuesta, numero: numero };
     console.log(req);
-    postData('http://echoun.herokuapp.com/histograma_ods', req).then( data => {
-        console.log(data); 
+    postData('http://echoun.herokuapp.com/histograma_ods', req).then(data => {
+        console.log(data);
         dibujar_burbujas(data, odss_res);
     });
 
-    postData('http://157.253.226.45:8000/sunburst', req).then(data => {
+    postData('http://echoun.herokuapp.com/sunburst', req).then(data => {
         dibujar_barritas(data);
     });
 })
--- scroll_manager.js
+++ scroll_manager.js
@@ -1,11 +1,28 @@
-var MOUSE_OVER = false;
+$('html, body').animate({
+    scrollTop: $("#sect" + 0).offset().top
+}, 300);   
+var act_sect = 0;
+var max_sect = 1;
 $('body').bind('mousewheel', function (e) {
-    var delta = e.wheelDelta;
-    if (delta > 0) {
-        console.log("subiendoo")
+    console.log(e);
+    if (e.originalEvent.deltaY > 0 && act_sect < max_sect) {
+        act_sect++;
+        $('#sect'+(act_sect)).css('visibility', 'visible');  
+        $('html, body').animate({
+            scrollTop: $("#sect" + act_sect).offset().top + 60
+        }, 300);
+        $('#sect'+(act_sect-1)).css('visibility', 'hidden');        
+       console.log('subiendooo')
     }
-    else {
-        console.log("bajandooo")
+    else if(e.originalEvent.deltaY < 0 && act_sect > 0){
+        act_sect--;
+        $('#sect'+(act_sect)).css('visibility', 'visible');  
+        $('html, body').animate({
+            scrollTop: $("#sect" + act_sect).offset().top + 60
+        }, 300);
+        $('#sect'+(act_sect+1)).css('visibility', 'hidden');  
+        console.log('bajandooo')
     }
+    console.log(act_sect)
 });
 
